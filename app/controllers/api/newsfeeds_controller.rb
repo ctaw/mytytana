14ec0bbe8ece7eeb29529f169617cef6a237c384
Charlene Lopez Taw <tawcharlene@gmail.com>
2015-03-02 23:28:10 +0800

    event date order

--- app/controllers/api/newsfeeds_controller.rb
+++ app/controllers/api/newsfeeds_controller.rb
@@ -2,7 +2,7 @@ class Api::NewsfeedsController < ApplicationController
   respond_to :xml, :json
 
   def index 
-    respond_with(@newsfeeds = Newsfeed.all)
+    respond_with(@newsfeeds = Newsfeed.all.order("event_date DESC"))
   end
   
 end
