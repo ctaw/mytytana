1ad1e9e677306fc0525fa2c326a0e032bef0a0ef
Charlene Lopez Taw <tawcharlene@gmail.com>
2015-03-05 22:20:36 +0800

    fixed

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -2,7 +2,7 @@ class Api::EventsController < ApplicationController
   respond_to :xml, :json
 
   def index 
-    respond_with(@events = Event.select("month, day, caption, updated_at").order("updated_at DESC"))
+    respond_with(@events = Event.select("month, day, caption").order("id DESC"))
   end
   
 end
