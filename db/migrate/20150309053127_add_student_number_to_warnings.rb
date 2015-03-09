dc641562fd847b22c3d219f77b5e183d5c64d82a
Charlene Lopez Taw <tawcharlene@gmail.com>
2015-03-09 16:19:19 +0800

    rm columns

--- db/migrate/20150309053127_add_student_number_to_warnings.rb
+++ db/migrate/20150309053127_add_student_number_to_warnings.rb
@@ -2,13 +2,13 @@ class AddStudentNumberToWarnings < ActiveRecord::Migration
   def change
     add_column :warnings, :student_number, :string
     remove_column :courses, :name, :string
-    remove_column, :subjects, :name, :string
+    remove_column :subjects, :name, :string
 
-    remove_column, :students, :gs_name, :string
-    remove_column, :students, :gs_location, :text
-    remove_column, :students, :gs_year, :string
-    remove_column, :students, :hs_name,  :string
-    remove_column, :students, :hs_location, :text
-    remove_column, :students, :hs_year, :string
+    remove_column :students, :gs_name, :string
+    remove_column :students, :gs_location, :text
+    remove_column :students, :gs_year, :string
+    remove_column :students, :hs_name,  :string
+    remove_column :students, :hs_location, :text
+    remove_column :students, :hs_year, :string
   end
 end
